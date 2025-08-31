50401ace062f4a24ec41dce81a478aabf723b5cf
Lauda Carly <lauda@nel.re>
2025-08-31 11:48:16 +0000

    Fix right click

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -63,6 +63,18 @@
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
+
+        # Move active window to workspace [0-9]
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
       ];
 
       bindm = [
@@ -71,6 +83,9 @@
       ];
 
       input = {
+        touchpad = {
+          clickfinger_behavior = 1;
+	};
         kb_layout = "dh";
 	kb_options = "caps:super,altwin:ctrl_win";
         follow_mouse = 0;
